22cf3a1d4964869d4e0c76bc73b494679dea9ead
phisit77 <kachidoki787@gmail.com>
2024-02-13 15:00:50 +0700

    first commit

---
+++ src/components/Wonder/WonderView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-3 mb-5 justify-content-md-start" v-for="item in allelder" :key="item.id">
+                <div class="card shadow p-0">
+                    <img :src="item.imageUrl" class="card-img-top" alt="elder">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ item.name }}</h5>
+                        <p class="card-text">Location - {{ item.location }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted,ref } from 'vue';
+
+const allelder = ref([]);
+
+onMounted(() => {
+    axios.get(`http://localhost:8080/elder`)
+        .then((response) => {
+            // console.log(response.data)
+            allelder.value = response.data
+        })
+})
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+toast.info("ยินดีต้อนรับแผนกเทคโนโลยีสารสนเทศ", {
+  autoClose: 1500,
+  position:toast.POSITION.TOP_CENTER
+})
+
+const notify = () => {
+  toast.success("ยินดีต้อนรับแผนกเทคโนโลยีสารสนเทศ", {
+    autoClose: 1500,
+    position: toast.POSITION.TOP_CENTER
+  })
+  return notify
+}
+
+</script>
+
+<template>
+  <div class="container my-5">
+    <div class="card shadow" style="width: 20rem;">
+      <div class="card-header p-0">
+        <img src="https://scontent.fbkk21-1.fna.fbcdn.net/v/t1.6435-9/123468338_198739328479048_8019884579575129978_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=uPWMyy8y0WQAX8AKJ7G&_nc_ht=scontent.fbkk21-1.fna&oh=00_AfCNdIsZxh1eTyPtGzwI7hBt5ICOCaEghSWH5inCsz1IhQ&oe=65F2A450" class="card-img-top"
+          alt="img">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Monster Hunter World</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary" @click="notify">Read...</a>
+      </div>
+    </div>
+  </div>
+</template>
